b4ba50b30cf32f606ec469adb9da5d4772730787
Kofo <kajotonis@gmail.com>
2021-12-13 18:23:24 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    
+    extend: {
+      colors:{
+        'blue': '#0d192b',
+        'midnight': 'hsl(216, 50%, 16%)'
+       },
+    },
+  },
+  plugins: [],
+}
